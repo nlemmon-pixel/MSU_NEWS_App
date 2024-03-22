10f749d6db14d3851882d88b1ff0154a8ba52527
Jason Kelley <108315590+Jason5618@users.noreply.github.com>
2024-03-22 15:00:44 -0500

    fixed merge issues to this branch

--- src/components/screens/athletics.js
+++ src/components/screens/athletics.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:src/components/screens/sports.js
 import React, { useState, useEffect } from "react";
 import {Top10Sports} from "../Hooks/top10/ArticleFetcher"; 
 
@@ -24,53 +23,3 @@ const Sports = () => {
 }
 
 export default Sports;
-=======
-import React, { useState, useEffect } from "react";
-
-const Athletics = () => {
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        const fetchArticles = async () => {
-            try {
-                //const response = await fetch("https://murraystatenews.org/wp-json/wp/v2/posts?category_name=athletics");
-                const response = await fetch("https://murraystatenews.org/wp-json/wp/v2/posts?categories=3");
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setArticles(data);
-            } catch (error) {
-                console.error('Error fetching articles:', error);
-            }
-        };
-
-        fetchArticles();
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Athletics screen</h1>
-            </header>
-            <main>
-                <section>
-                    <h2>Breaking News TEST</h2>
-                    {articles.map(article => (
-                        <div key={article.id}>
-                            <h3>{article.title.rendered}</h3>
-                            <p>{article.excerpt.rendered}</p>
-                            <h1 style={{color:"#0000FF"}}>{article.categories.map(category => (<div>{category} </div>))}</h1>
-                        </div>
-                    ))}
-                </section>
-            </main>
-            <footer>
-                &copy; 2024 Your News App. All rights reserved.
-            </footer>
-        </div>
-    );
-}
-
-export default Athletics;
->>>>>>> Stashed changes:src/components/screens/athletics.js
--- src/components/screens/home.js
+++ src/components/screens/home.js
@@ -1,10 +1,6 @@
 import React from "react";
 //import Recent from "../Hooks/top10/top10recent";
-<<<<<<< Updated upstream
 import {Top10Recent} from "../Hooks/top10/ArticleFetcher";
-=======
-import {Top10Recent} from "../Hooks/top10/top10recent";
->>>>>>> Stashed changes
 
 const Home = () => {
 
@@ -17,12 +13,7 @@ const Home = () => {
                 <section>
                     <h2>Breaking News</h2>
                     <hr/>
-<<<<<<< Updated upstream
-                    <Top10Recent />
-                    {/*<Recent/>*/}
-=======
                     <Top10Recent/>
->>>>>>> Stashed changes
                 </section>
             </main>
             <footer>
