996c78d2a35b9bf341972cad5257c5503ffd312e
nlemmon-pixel <142365055+nlemmon-pixel@users.noreply.github.com>
2024-04-12 16:44:52 -0500

    updated css and formatting
    search bar, footer, and header

--- src/components/Hooks/Constants/footer.js
+++ src/components/Hooks/Constants/footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './FHstyles.css'; // Import the CSS stylesheet
+import IconTrophy from '../../../icons/whiteTrophies.png'
+import IconHome from '../../../icons/whiteHome.png'
+import IconNews from '../../../icons/whiteNews.png'
+import IconMore from '../../../icons/whiteMores.png'
+import IconLifestyle from '../../../icons/whiteLifestyle.png'
 
 const Footer = () => {
     const navigate = useNavigate();
@@ -13,11 +18,11 @@ const Footer = () => {
             <button className="navButton" onClick={() => navigate('/athletics')}>Athletics</button>
             <button className="navButton" onClick={() => navigate('/help')}>Help</button>*/}
 
-            <button className="navButton" onClick={() => navigate('/athletics')}><img src="" alt="Athletics"/></button> 
-            <button className="navButton" onClick={() => navigate('/features')}><img src="" alt="Features"/></button> 
-            <button className="navButton" onClick={() => navigate('/')}><img src="" alt="Home"/></button> 
-            <button className="navButton" onClick={() => navigate('/opinion')}><img src="" alt="Opinion"/></button> 
-            <button className="navButton" onClick={() => navigate('/settings')}><img src="" alt="Settings"/></button> {/* settings - replace with a dropdown for more, which will contain settings */}
+            <button className="navButton" onClick={() => navigate('/athletics')}><img src={IconTrophy} alt="Athletics"/></button> 
+            <button className="navButton" onClick={() => navigate('/features')}><img src={IconLifestyle} alt="Features"/></button> 
+            <button className="navButton" onClick={() => navigate('/')}><img src={IconHome} alt="Home"/></button> 
+            <button className="navButton" onClick={() => navigate('/opinion')}><img src={IconNews} alt="Opinion"/></button> 
+            <button className="navButton" onClick={() => navigate('/settings')}><img src={IconMore} alt="Settings"/></button> {/* settings - replace with a dropdown for more, which will contain settings */}
         </nav>
     );
 }
